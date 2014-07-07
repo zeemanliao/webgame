0672f25ca0dd691c29162134d558dc7cc696be9c
zeemanliao <zeemanliao@gmail.com>
2014-07-07 16:53:13 +0800

    update

--- views/js/event.cjs
+++ views/js/event.cjs
@@ -4,8 +4,12 @@
 	});
 
 	socket.on('show error',function(data){
+		o.message.show(data.msg,false);
+	});
+
+	socket.on('show error2',function(data){
 		debug('Error = >');
-		debug(data);
+		debug(data.msg);
 		o.message.show('程式出現非預期錯誤',false);
 	});
 
@@ -21,4 +25,3 @@
   socket.on('check ver', function(data){
     socket.emit('check ver',db.ver);
   });
-
