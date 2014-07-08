0bc0f5507f0f345e3aa650356985b152aec47148
zeemanliao <zeemanliao@gmail.com>
2014-07-08 16:55:16 +0800

    update

--- app.js
+++ app.js
@@ -1,49 +1,24 @@
 /**
  * Module dependencies.
  */
-var game = require('./lib/game');
-var cluster = require('cluster'),
-    express = require('express'),
-    routes = require('./routes'),
-    socket_route = require('./lib/route'),
-    settings = require('./lib/settings'),
-    cookie = express.cookieParser(settings.cookie_secret),
-    store = new express.session.MemoryStore(),
-    session = express.session({
-        key: settings.cookie_key,
-        secret: settings.cookie_secret,
-        store: store
-    }),
-    http = require('http'),
-    path = require('path'),
-    app = express(),
-    cpuCount = require('os').cpus().length;
 
-if (cluster.isMaster) {
 
-    // Create a worker for each CPU
-    //for (var i = 0; i < cpuCount; i += 1) {
-    for (var i = 0; i < 1; i += 1) {
-        cluster.fork();
-    }
-    // Listen for dying workers
-    cluster.on('fork', function(worker) {
-
-        // Replace the dead worker,
-        // we're not sentimental
-        console.log('Worker A ' + worker.id + ',pid=' + worker.process.pid + '.............fork');
-    });
-    // Listen for dying workers
-    cluster.on('exit', function(worker, code, signal) {
-
-        // Replace the dead worker,
-        // we're not sentimental
-        console.log('Worker B ' + worker.id + ',pid=' + worker.process.pid + '.............died');
-        cluster.fork();
-    });
-
-} else {
+    var express = require('express'),
+        routes = require('./routes'),
+        socket_route = require('./lib/route'),
+        settings = require('./lib/settings'),
+        cookie = express.cookieParser(settings.cookie_secret),
+        store = new express.session.MemoryStore(),
+        session = express.session({
+            key: settings.cookie_key,
+            secret: settings.cookie_secret,
+            store: store
+        }),
+        http = require('http'),
+        path = require('path'),
+        app = express();
     // all environments
+
     app.set('port', process.env.PORT || 1234);
     app.set('views', __dirname + '/views');
     app.set('view engine', 'ejs');
@@ -63,15 +38,22 @@ if (cluster.isMaster) {
     }
 
     routes(app);
+    app.listen(1234);
 
+    // Configuracoes do Socket.IO
+     
+    var RedisStore = require('socket.io/lib/stores/redis');
+    var redis      = require('socket.io/node_modules/redis');
     var io = require('socket.io').listen(
-        http.createServer(app).listen(app.get('port'), function() {
-            console.log('Express server listening on port ' + app.get('port'));
-        }), {
-            log: true
+        http.createServer(app).listen(app.get('port'), 
+            function() {
+                console.log('Express And Game server listening on port ' + app.get('port'));
+            })
+        , {
+          'log': true
         }
     );
-    // Configuracoes do Socket.IO
+
     io.set('authorization', function(data, accept) {
         cookie(data, {}, function(err) {
             if (!err) {
@@ -90,6 +72,4 @@ if (cluster.isMaster) {
         });
     });
 
-    game.load(io.sockets);
-    socket_route(io, game);
-}
+    socket_route(io);
