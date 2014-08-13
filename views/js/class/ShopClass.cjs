28fd6b028461cf4d95288b2cdb965911a5a03fab
zeemanliao <zeeman.liao@gmail.com>
2014-08-13 22:59:44 +0800

    update

--- views/js/class/ShopClass.cjs
+++ views/js/class/ShopClass.cjs
@@ -1,20 +1,78 @@
 
+
+/*
+***********************************************************************************************
+============================================商店子元件===========================================
+***********************************************************************************************
+*/
+function ShopClass(data) {
+  var self = this;
+
+  this.frame = $('#' + data.frameName);
+  this.buttonName = data.buttonName;
+  this.com2 = data.com2;
+  this.checkShow = data.checkShow;
+
+  live('.'+this.com2+'_button',{
+    click:function (event) {
+
+      var item = o.storage.bag[settings.storageType.bag].items[$(this).attr('data')];
+
+      if (!item)
+        return ;
+      
+      if (item.base.coins * item.data.level > parseInt(o.chara.data.gold)) {
+        o.message.show('您的金錢不足「'+item.coins+'」<br><br>無法升級「'+item.nam+'」');
+      } else {
+        coms.item.emit(self.com2, item.id);
+      }
+    }
+  });
+}
+
+ShopClass.prototype.clear = function() {
+  this.frame.empty();
+}
+
+ShopClass.prototype.show = function() {
+  this.frame.show();
+}
+ShopClass.prototype.hide = function() {
+  this.frame.hide();
+}
+ShopClass.prototype.reflush = function() {
+  this.clear();
+  var items = o.storage.bag[settings.storageType.bag].items;
+  for (var i in items) {
+    var item = items[i];
+    if (this.checkShow(item, items)){
+    this.frame.append('<li id="item'+item.id+'">'+
+              '<div class="label" gid="nam">'+'lv.'+item.data.level+item.base.nam+'</div>'+
+              '<div gid="attr">'+tool.getAttr(item)+'</div>'+
+              '<div class="number" gid="mixInfo">x'+item.data.num+'<br>$'+(item.data.level * item.base.coins)+'</div>'+
+              '<btn class="'+this.com2+'_button" data="'+item.id+'">'+this.buttonName+'</btn>'+
+              '</li>');
+    }
+  }
+}
+
+
 /*
 ***********************************************************************************************
 ============================================商店元件===========================================
 ***********************************************************************************************
 */
-var ShopClass = Object.Extend(BaseViewClass);
+var ShopFrameClass = Object.Extend(BaseViewClass);
 
-ShopClass.prototype.initialize = function(){
+ShopFrameClass.prototype.initialize = function(){
   this.base();
   this.version = 0;
 }
 
-ShopClass.prototype.create = function(){
+ShopFrameClass.prototype.create = function(){
   var self = this;
   this.frame = $('#shop_frame');
-  this.content = $('.city_content','#shop_frame');
+  this.content = this.frame.find('.city_content');
   this.itemList = this.content.find('.itemList');
   live('.buyItem_button',{
     click:function (event) {
@@ -30,7 +88,7 @@ ShopClass.prototype.create = function(){
   });
  }
 
-ShopClass.prototype.update=function(){
+ShopFrameClass.prototype.update=function(){
 	if (this.version != publicData.version.items) {
 		this.version = publicData.version.items;
 		this.itemList.empty();
